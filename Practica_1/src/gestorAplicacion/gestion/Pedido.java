7e9b9f94e993607b10ce49d0d3f0694b7749583e
Sergio Arias <sarias@unal.edu.co>
2022-10-12 15:42:30 -0500

    sergio

--- Practica_1/src/gestorAplicacion/gestion/Pedido.java
+++ Practica_1/src/gestorAplicacion/gestion/Pedido.java
@@ -13,7 +13,6 @@ public class Pedido {
     private static long numeroPedido=0;
     //constructor
     Pedido(){
-        1
         this.codigo=generarCodigo();
     }
     public long generarCodigo(){
