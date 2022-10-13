99bd6b664b6be4ade19d63606acb33b2bd5b7c94
russbelln <rnorena@unal.edu.co>
2022-10-13 12:15:05 -0500

    refresh

---
+++ Practica_1/src/uiMain/Funcionalidades.java
@@ -0,0 +1,24 @@
+package uiMain;
+import java.util.Scanner;
+
+public class Funcionalidades {
+    static Scanner sc = new Scanner(System.in);
+    public static void generarInformeVentas(){
+        System.out.println("A continuacion ingrese ingrese las opciones de acuerdo a los datos requeridos en su informe:\n");
+        System.out.println("Ingrese la fecha inicial de su informe");
+        int inicial = sc.nextInt();
+        while(inicial < 0){
+            System.out.println("Por favor ingrese una fecha valida");
+            inicial = sc.nextInt();
+        }
+        System.out.println("Ingrese la fecha final de su informe");
+        int fin = sc.nextInt();
+        while (fin<=inicial){ // la fecha final del informe debe ser mayor que la fecha inicial.
+            System.out.println("Por favor ingrese valida");
+            fin = sc.nextInt();
+        }
+
+
+
+    }
+}
