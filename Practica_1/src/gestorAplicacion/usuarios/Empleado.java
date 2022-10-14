7d6b76714bbf10827c2aa1ce0e1812f06bd8cabd
Sergio Arias <sarias@unal.edu.co>
2022-10-14 14:20:05 -0500

    sergio

--- Practica_1/src/gestorAplicacion/usuarios/Empleado.java
+++ Practica_1/src/gestorAplicacion/usuarios/Empleado.java
@@ -1,14 +1,24 @@
 package gestorAplicacion.usuarios;
 
+import java.util.ArrayList;
+
 public abstract class Empleado extends Usuario{
+    //ATRIBUTOS DE INSTANCIA
     protected String cargo;
     protected int salario;
     protected boolean estadoIngreso;
 
+    //ATRIBUTOS DE CLASE
+
+    private static ArrayList<Empleado> empleados= new ArrayList<>();
+    //CONSTRUCTOR
     Empleado(long codigo, String nombre) {
         super(codigo, nombre);
+        empleados.add(this);
     }
 
+
+    //METODOS ABSTRACTOS
     public abstract String asegurar();
 
 
---
+++ Practica_1/src/gestorAplicacion/usuarios/Sueldo.java
@@ -0,0 +1,6 @@
+package gestorAplicacion.usuarios;
+
+public interface Sueldo {
+    public final static double sueldo=100;
+    
+}
