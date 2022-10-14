1cc82f276b2ba4e5a5c29920ebc25c4b08231af5
Sergio Arias <sarias@unal.edu.co>
2022-10-14 16:15:01 -0500

    sergio quiero terminar esto

--- Practica_1/src/gestorAplicacion/usuarios/Empleado.java
+++ Practica_1/src/gestorAplicacion/usuarios/Empleado.java
@@ -1,22 +1,94 @@
 package gestorAplicacion.usuarios;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public abstract class Empleado extends Usuario{
     //ATRIBUTOS DE INSTANCIA
     protected String cargo;
-    protected int salario;
+    protected Double sueldo;
     protected boolean estadoIngreso;
 
+    protected Double comision;
+
+    protected LocalDate fechaCreacion; //fecha cuando se creo el empleado
+
+    protected LocalDate fechaIngreso; //fecha cuando ingreso el empleado
     //ATRIBUTOS DE CLASE
 
     private static ArrayList<Empleado> empleados= new ArrayList<>();
+
     //CONSTRUCTOR
-    Empleado(long codigo, String nombre) {
+
+    public Empleado(long codigo, String nombre, String cargo, Double sueldo, boolean estadoIngreso, Double comision, LocalDate fechaCreacion, LocalDate fechaIngreso) {
         super(codigo, nombre);
-        empleados.add(this);
+        this.cargo = cargo;
+        this.sueldo = sueldo;
+        this.estadoIngreso = estadoIngreso;
+        this.comision = comision;
+        this.fechaCreacion = LocalDate.now();
+        this.fechaIngreso = fechaIngreso;
     }
 
+
+    //GETTERS Y SETTERS
+
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public boolean isEstadoIngreso() {
+        return estadoIngreso;
+    }
+
+    public void setEstadoIngreso(boolean estadoIngreso) {
+        this.estadoIngreso = estadoIngreso;
+    }
+
+    public Double getComision() {
+        return comision;
+    }
+
+    public void setComision(Double comision) {
+        this.comision = comision;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public LocalDate getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(LocalDate fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public static ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public static void setEmpleados(ArrayList<Empleado> empleados) {
+        Empleado.empleados = empleados;
+    }
 
     //METODOS ABSTRACTOS
     public abstract String asegurar();
--- Practica_1/src/uiMain/Funcionalidades.java
+++ Practica_1/src/uiMain/Funcionalidades.java
@@ -1,9 +1,17 @@
 package uiMain;
+
+import java.time.LocalDate;
+import java.util.*;
+import static java.util.Map.entry;
+import java.time.temporal.ChronoField;
+
 import gestorAplicacion.gestion.Administracion;
 import gestorAplicacion.gestion.Pedido;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import gestorAplicacion.usuarios.*;
+import gestorAplicacion.gestion.*;
 
 public class Funcionalidades {
 
@@ -18,6 +26,17 @@ public class Funcionalidades {
 
 
 
+
+    }
+
+    public static void contabilidad(){
+        System.out.println("Bienvenido al menu de contabilidad");
+
+        System.out.println("Meses disponibles");
+
+        //se buscan meses en los que se han comprado productos y
+        SortedSet<Integer> fechas =new TreeSet<Integer>();//meses unicos
+        for (Producto productos: )
 
     }
 }
