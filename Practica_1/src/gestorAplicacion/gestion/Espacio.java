2291643e25703c3d4920b0fa4723b6f909b9669e
russbelln <rnorena@unal.edu.co>
2022-10-14 16:16:19 -0500

    refresh

--- Practica_1/src/gestorAplicacion/gestion/Espacio.java
+++ Practica_1/src/gestorAplicacion/gestion/Espacio.java
@@ -1,12 +1,39 @@
 package gestorAplicacion.gestion;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
+
 
 public class Espacio {
+    //ATRIBUTOS DE INSTANCIA
     private String nombre;
+
+    private ArrayList<LocalDate> fechas = new ArrayList(); // fechas disponibles para un espacio
+    // ATRIBUTOS DE CLASE
     private static ArrayList<Espacio> listado = new ArrayList<>();
-    private HashMap<String, String> disponibilidadFecha = new HashMap<>();
 
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public static ArrayList<Espacio> getListado() {
+        return listado;
+    }
+
+    public static void setListado(ArrayList<Espacio> listado) {
+        Espacio.listado = listado;
+    }
+
+    public ArrayList<LocalDate> getFechas() {
+        return fechas;
+    }
+
+    public void setFechas(ArrayList<LocalDate> fechas) {
+        this.fechas = fechas;
+    }
 }
--- Practica_1/src/uiMain/Funcionalidades.java
+++ Practica_1/src/uiMain/Funcionalidades.java
@@ -1,7 +1,9 @@
 package uiMain;
 import gestorAplicacion.gestion.Administracion;
+import gestorAplicacion.gestion.Espacio;
 import gestorAplicacion.gestion.Pedido;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,10 +13,37 @@ public class Funcionalidades {
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido al menu para realizar una Reserva");
         System.out.println("Por favor escoja un sitio a reservar disponible");
-        //ArrayList<Espacio> espaciosDisponibles = usuario.getEspacios.getNombre;
-        for (int i = 0; i < 10; i++) { // ciclo para mostrar los espacios disponibles
-
+        ArrayList<Espacio> espaciosDisponibles = Espacio.getListado();
+        for (int i = 0; i < espaciosDisponibles.size(); i++) { // ciclo para mostrar los espacios disponibles
+            System.out.println(i+". "+espaciosDisponibles.get(i).getNombre());
         }
+        System.out.println("Ingrese su opcion: ");
+        int opcion = sc.nextInt();
+        while(opcion<0 || opcion >=(espaciosDisponibles.size())){
+            System.out.println("Por favor ingrese una opcion valida: ");
+            opcion = sc.nextInt();
+        }
+
+        Espacio espacioTomado = espaciosDisponibles.get(opcion); // seleciona el espacio del atributo de clase espaciosDisponibles
+
+
+        System.out.println("A continuacion le presentamos la disponibildiad del espacio: ");
+
+        for (int i = 0; i < 10; i++) { // imprime los primeros 10 dias disponibles
+            System.out.println(i+". "+espacioTomado.getFechas().get(i));
+        }
+        System.out.println("Escoja una opcion ");
+        opcion = sc.nextInt();
+        while(opcion<0 || opcion >= 10){
+            System.out.println("Por favor ingrese una opcion valida: ");
+            opcion = sc.nextInt();
+        }
+        LocalDate fechaTomada = espacioTomado.getFechas().get(opcion);
+        espacioTomado.getFechas().remove(opcion); // se quita la disponibilidad de la fecha seleccionada
+        System.out.println("Desea agregar servicios adicionales a la reserva? ");
+        System.out.println("1. Si ");
+        System.out.println("2. No ");
+        opcion = sc.nextInt();
 
 
 
