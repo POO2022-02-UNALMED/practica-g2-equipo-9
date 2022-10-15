068bc613b7e88125824300e195658ba9b28ea146
Sergio Arias <sarias@unal.edu.co>
2022-10-14 23:50:48 -0500

    1

--- Practica_1/src/uiMain/Funcionalidades.java
+++ Practica_1/src/uiMain/Funcionalidades.java
@@ -1,5 +1,6 @@
 package uiMain;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.util.*;
 import java.time.temporal.ChronoField;
@@ -13,17 +14,17 @@ import gestorAplicacion.gestion.*;
 
 public class Funcionalidades {
 
-    public static void realizarReserva(){
+    public static void realizarReserva() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido al menu para realizar una Reserva");
         System.out.println("Por favor escoja un sitio a reservar disponible");
         ArrayList<Espacio> espaciosDisponibles = Espacio.getListado();
         for (int i = 0; i < espaciosDisponibles.size(); i++) { // ciclo para mostrar los espacios disponibles
-            System.out.println(i+". "+espaciosDisponibles.get(i).getNombre());
+            System.out.println(i + ". " + espaciosDisponibles.get(i).getNombre());
         }
         System.out.println("Ingrese su opcion: ");
         int opcion = sc.nextInt();
-        while(opcion<0 || opcion >=(espaciosDisponibles.size())){
+        while (opcion < 0 || opcion >= (espaciosDisponibles.size())) {
             System.out.println("Por favor ingrese una opcion valida: ");
             opcion = sc.nextInt();
         }
@@ -34,11 +35,11 @@ public class Funcionalidades {
         System.out.println("A continuacion le presentamos la disponibildiad del espacio: ");
 
         for (int i = 0; i < 10; i++) { // imprime los primeros 10 dias disponibles
-            System.out.println(i+". "+espacioTomado.getFechas().get(i));
+            System.out.println(i + ". " + espacioTomado.getFechas().get(i));
         }
         System.out.println("Escoja una opcion ");
         opcion = sc.nextInt();
-        while(opcion<0 || opcion >= 10){
+        while (opcion < 0 || opcion >= 10) {
             System.out.println("Por favor ingrese una opcion valida: ");
             opcion = sc.nextInt();
         }
@@ -50,36 +51,52 @@ public class Funcionalidades {
         opcion = sc.nextInt();
 
 
-
-
     }
 
-    public static String contabilidad(){
+    public static void contabilidad() {
+        Scanner entrada = new Scanner(System.in);
         System.out.println("Bienvenido al menu de contabilidad");
 
         System.out.println("Meses disponibles");
 
-        //se buscan meses en los que se han comprado productos y
-        SortedSet<Integer> fechas =new TreeSet<Integer>();//meses unicos
-        for (Producto producto: Producto.getProductos()){
-            fechas.add(producto.getFechaVenta().get(ChronoField.MONTH_OF_YEAR));
+        //se buscan meses en los que se han comprado productos
+        //tanteando cosas XD
+        //Sorted set almacena las fechas de forma ordenada y no admite repetidos, lo hace automaticamente
+        SortedSet<Integer> fechas = new TreeSet<Integer>();//meses unicos
+        for (Producto producto : Producto.getProductos()) {
+            //busca la fecha de venta de producto, luego obtiene de ella el int asociado al mes y lo guarda en fechas
+            fechas.add(producto.getFechaVenta().getMonthValue());
         }
         HashMap<Integer,String> meses = new HashMap<Integer,String>();
         meses.put(1,"Enero"); meses.put(2,"Febrero"); meses.put(3, "Marzo"); meses.put(4, "Abril"); meses.put(5, "Mayo"); meses.put(6,"Junio");
         meses.put(7, "Julio");meses.put(8, "Agosto"); meses.put(9, "Septiembre"); meses.put(10, "Octubre"); meses.put(11, "Noviembre"); meses.put(12, "Diciembre");
 
-        Iterator value=fechas.iterator();
-        int i=0;
-        return (String) value.next();
+        //Iterator lo uso para recorrer el Array fechas de forma secuencial
+        Iterator value = fechas.iterator(); //El método iterator() nos va a permitir obtener un objeto de tipo Iterator que representa la colección a recorrer
+        int i= 1;
+        //.hasNext: Método que devuelve true si existe un siguiente elemento a la hora de iterar sobre una colección.
+        while (value.hasNext()) {
+            //aqui se muestran las fechas de las que se tiene conocimiento de ventas realizadas
+            System.out.println("["+i+"]"+" "+meses.get(value.next())+" de "+2022);
+            i++;
+        }
+        System.out.println("Por favor ingresa una opcion");
+        int opcion=entrada.nextInt();
+        
+
+
+
+
 
 
     }
 
-    public static String main(String[] args) {
-        Producto producto=new Producto(null,null,"vendido","coca",0,0,null,null, LocalDate.of(2020,02,02),0,0);
-        Producto producto1=new Producto(null,null,"vendido","coca",0,0,null,null, LocalDate.of(2020,02,02),0,0);
-        Producto producto2=new Producto(null,null,"vendido","coca",0,0,null,null, LocalDate.of(2020,02,1),0,0);
-        System.out.println(contabilidad());
-
+    public static void main(String[] args) {
+        Producto producto = new Producto(null, null, "vendido", "coca", 0, 0, null, null, LocalDate.of(2020, 02, 02), 0, 0);
+        Producto producto1 = new Producto(null, null, "vendido", "coca", 0, 0, null, null, LocalDate.of(2020, 02, 02), 0, 0);
+        Producto producto2 = new Producto(null, null, "vendido", "coca", 0, 0, null, null, LocalDate.of(2020, 02, 1), 0, 0);
+        Producto producto3 = new Producto(null, null, "vendido", "coca", 0, 0, null, null, LocalDate.of(2020, 12, 1), 0, 0);
+        contabilidad();
     }
 }
+
