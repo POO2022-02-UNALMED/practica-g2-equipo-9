5f98033255a73fe4ba85dc7f5589b8b59576ae88
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-10-18 01:12:48 -0500

    V

--- Practica_1/src/gestorAplicacion/gestion/Servicio.java
+++ Practica_1/src/gestorAplicacion/gestion/Servicio.java
@@ -1,6 +1,6 @@
 package gestorAplicacion.gestion;
 
-public enum Servicio {
+public enum Servicio implements Serializable{
     BARMAN(200),  COCKTAILCHALLENGE(300),MESEROEXCLUSIVO(400), STRIPPER(500);
 
     private final float precio;
@@ -10,5 +10,23 @@ public enum Servicio {
 
     public float getPrecio() {
         return precio;
+    }
+
+    //SERIALIZACION
+    try{
+        ObjectOutputStream servicios_datos = new ObjectOutputStream(new FileOutputStream("/servicio.dat"));
+        
+        servicio_datos.writeObject(precio);
+        
+        servicio_datos.close();
+        
+        ObjectInputStream servicio_recuperar= new ObjectInputStream(new FileInputStream("/servicio.dat"));
+        
+        //DEVUELVE LOS DATOS 
+        servicio_datos.writeObject(precio);
+        
+        ventas_recuperar.close();
+    }
+    catch (Exception precio){
     }
 }
