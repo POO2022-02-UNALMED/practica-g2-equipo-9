0fd755362b20332af629f636cdfbf24a5871c8a7
kquinones107 <kquinones@unal.edu.co>
2022-11-16 18:03:36 -0500

    rrr

--- Practica_2/src/gestorAplicacion/gestion/Pedido.py
+++ Practica_2/src/gestorAplicacion/gestion/Pedido.py
@@ -15,7 +15,11 @@ def __init__(self, cliente = None,  estadoPedido = "", productos = 0, servicio =
     Pedido._pedidos.append(self)
     Pedido._totalPedidos += 1
 
-    
+    def getTrabajador(self):
+        return self._trabajador
+
+    def setTrabajador(self, trabajador):
+        self._trabajador = trabajador
 
 
     def getCliente(self):
@@ -29,3 +33,47 @@ def __init__(self, cliente = None,  estadoPedido = "", productos = 0, servicio =
 
     def setFechaPedido(self, fechaPedido):
         self._fechaPedido = fechaPedido
+
+    def getEstadoPedido(self):
+        return self._estadoPedido
+
+    def setEstadoPedido(self, estadoPedido):
+        self._estadoPedido = estadoPedido
+
+    def getProductos(self):
+        return self._productos
+
+    def setProductos(self, productos):
+        self._productos = productos
+
+    def getServicio(self):
+        return self._servicio
+
+    def setServicio(self, servicio):
+        self._servicio = servicio
+
+    @classmethod
+    def getPedidos(cls):
+        return cls._pedidos
+
+    @classmethod
+    def setPedidos(cls, pedidos):
+        cls._pedidos = pedidos
+
+    @classmethod
+    def getTotalPedidos(cls):
+        return cls._totalPedidos
+
+    @classmethod
+    def setTotalPedidos(cls, totalPedidos):
+        cls._totalPedidos = totalPedidos
+
+    @classmethod
+    def getNumeroPedido(cls):
+        return cls._numeroPedido
+
+    @classmethod
+    def setNumeroPedido(cls, numeroPedido):
+        cls._numeroPedido = numeroPedido
+
+    
